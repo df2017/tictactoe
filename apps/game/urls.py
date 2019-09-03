52f1653167262099d571374bb9346e91ad5d98a9
Diego Ferreyra <dferreyra@vates.org>
2019-09-03 17:42:07 -0300

    probando vista board

---
+++ apps/game/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from .views import BoardView
+
+
+urlpatterns = [
+    path('board/', BoardView.as_view(), name='board'),
+
+]
--- apps/game/views.py
+++ apps/game/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+class BoardView(TemplateView):
+      template_name = 'board/board.html'
+      success_url = '/'
+
+      def get_context_data(self, **kwargs):
+          ctx = super(BoardView, self).get_context_data(**kwargs)
+          ctx['header'] = ['A', 'B', 'C']
+          ctx['rows'] = [{'A':'X', 'B': 'O', 'C': '-'},
+                         {'A':'X','B': '-', 'C': '-'},
+                         {'A':'-', 'B': '-', 'C': '-'}]
+          return ctx
