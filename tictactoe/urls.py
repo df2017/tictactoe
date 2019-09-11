af45b53be03ed004f96c903ac62c8b37b9992682
df2017 <dferrey22783@gmail.com>
2019-09-11 18:06:31 -0300

    Update urls.py

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rest_framework.urls')),
+    path('api/', include('rest_framework.urls')),
     path('', include('apps.game.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
