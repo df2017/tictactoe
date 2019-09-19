b9526b315faf45ec1558ffa723f9b7e91f6fc0e9
df2017 <dferrey22783@gmail.com>
2019-09-19 13:43:36 -0300

    Update apiview.py

--- apps/game/apiview.py
+++ apps/game/apiview.py
@@ -5,13 +5,26 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Board, Move, Player
 
+
 class PlayerList(APIView):
     def get(self, request):
-        list = Player.objects.all()[:2]
+        list = Player.objects.all()
         data = PlayerSerializer(list, many=True).data
         return Response(data)
 
+    def post(self, request):
+        serializer = PlayerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class PlayerApiUpdate(APIView):
+
+    def get(self, request, id):
+        detail = get_object_or_404(Player, pk=id)
+        data = PlayerSerializer(detail).data
+        return Response(data)
 
     def put(self, request, id):
         position = get_object_or_404(Player, pk=id)
