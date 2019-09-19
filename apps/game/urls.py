4607fc8d15ec1d70c7d3ef572653f8e6030236d4
df2017 <dferrey22783@gmail.com>
2019-09-19 13:49:01 -0300

    Update urls.py

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     path('board/', BoardView.as_view(), name='board'),
     path('move/<int:mov>/', move, name='move'),
     path('reset/', reset, name='reset'),
-    path('api_game/', PlayerList.as_view(), name='apiplayerget'),
+    path('api_game/player/', PlayerList.as_view(), name='apiplayerget'),
+    path('api_game/player/<int:id>/', PlayerApiUpdate.as_view(), name='apiplayerput'),
     path('api_game/list/', BoardList.as_view(), name='apilistget'),
     path('api_game/list/<int:pk>/', BoardDetail.as_view(), name="apidetailget"),
     path('api_game/position/<int:id>/', BoardApiUpdate.as_view(), name='apipositionput'),
     path('api_game/move/', MoveList.as_view(), name='apipmovelistget'),
     path('api_game/move/<int:pk>/', MoveDetail.as_view(), name='apipmoveget'),
-    path('api_game/<int:id>/', PlayerApiUpdate.as_view(), name='apiplayerput'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
